65b4e65323192fe7ce4226963174c1eb85e29f83
Stephen Wong <stephen.hlx+github@gmail.com>
2023-10-27 16:16:07 -0700

    Use text block to replace string concatenation

--- lib/src/test/java/overcooked/visual/JgraphtDotGraphExporterTest.java
+++ lib/src/test/java/overcooked/visual/JgraphtDotGraphExporterTest.java
@@ -16,13 +16,15 @@ class JgraphtDotGraphExporterTest {
         .export(ImmutableSet.of(
             TRANSITION_0_2,
             TRANSITION_2_3)))
-        .isEqualTo("strict digraph G {\n"
-            + "  S_0 [ label=\"actor1(f1=1,f2=0), actor2(f1=2,f2=0), actor3(f1=3,f2=0), actor4(f1=4,f2=0)\" ];\n"
-            + "  S_2 [ label=\"actor1(f1=1,f2=0), actor2(f1=2,f2=1), actor3(f1=3,f2=1), actor4(f1=4,f2=0)\" ];\n"
-            + "  S_3 [ label=\"actor1(f1=1,f2=0), actor2(f1=2,f2=1), actor3(f1=3,f2=0), actor4(f1=4,f2=0)\" ];\n"
-            + "  S_0 -> S_2 [ label=\"actor2.actor2.method1(actor3)\" ];\n"
-            + "  S_2 -> S_3 [ label=\"actor3.actor3.method1()\" ];\n"
-            + "}\n");
+        .isEqualTo("""
+            strict digraph G {
+              S_0 [ label="actor1(f1=1,f2=0), actor2(f1=2,f2=0), actor3(f1=3,f2=0), actor4(f1=4,f2=0)" ];
+              S_2 [ label="actor1(f1=1,f2=0), actor2(f1=2,f2=1), actor3(f1=3,f2=1), actor4(f1=4,f2=0)" ];
+              S_3 [ label="actor1(f1=1,f2=0), actor2(f1=2,f2=1), actor3(f1=3,f2=0), actor4(f1=4,f2=0)" ];
+              S_0 -> S_2 [ label="actor2.actor2.method1(actor3)" ];
+              S_2 -> S_3 [ label="actor3.actor3.method1()" ];
+            }
+            """);
   }
 
 }
