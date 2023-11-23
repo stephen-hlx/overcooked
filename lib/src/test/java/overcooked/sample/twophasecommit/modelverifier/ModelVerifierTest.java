b42e5cf56e924ae510fb24499c2eaef2a8780d78
Stephen Wong <stephen.hlx+github@gmail.com>
2023-11-23 12:23:29 -0800

    Fix checkstyle

--- lib/src/test/java/overcooked/sample/twophasecommit/modelverifier/ModelVerifierTest.java
+++ lib/src/test/java/overcooked/sample/twophasecommit/modelverifier/ModelVerifierTest.java
@@ -1,11 +1,9 @@
 package overcooked.sample.twophasecommit.modelverifier;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ModelVerifierTest {
   @Test
-//  @Disabled
   void can_run_without_error() {
     new ModelVerifier().verify();
   }
